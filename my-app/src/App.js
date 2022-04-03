ed7314fc2b44a2755f272d85300570d03d6f9b6f
Beth Jackson <beth.jack@gmail.com>
2022-04-03 13:52:21 +0200

    Roughed out a card to display

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import image1 from "./image1.svg";
+import image2 from "./image2.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <img className="speech-marks" src={image1} alt="Closing speech marks" />
+        <h2 className="statement">
+          This piece of software is{" "}
+          <span className="green-text" style={{ fontStyle: "italic" }}>
+            the shit
+          </span>
+          ,
+          <br />
+          I've never tried anything like it.
+        </h2>
+        <img className="image2" src={image2} alt="Mr Face emoji" />
+        <span className="image2-title">Mr. Face</span>
+        <span className="image2-subtext">Karma Guru @ Face Co&trade;</span>
+      </div>
+      <div className="progress-bar"></div>
     </div>
   );
 }
